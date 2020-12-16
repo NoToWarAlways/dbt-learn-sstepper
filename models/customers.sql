74e9b7ca674e21e0785e60360f9e8693a1554e05
Simon Stepper <simon.stepper@proglove.de>
2020-12-16 16:18:18 +0000

    added orders table and customer lifetime value

--- models/customers.sql
+++ models/customers.sql
@@ -6,19 +6,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('orders') }}
 
 ),
 
@@ -29,7 +17,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
@@ -46,7 +35,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    
 
     from customers
 
---
+++ models/orders.sql
@@ -0,0 +1,35 @@
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+successful_payments as (
+
+    select * from {{ ref('stg_payments') }}
+    where status = 'success'
+),
+
+payments_aggregated as (
+    
+    select 
+        order_id,
+        sum(amount) as amount
+    
+    from successful_payments
+    group by 1
+),
+
+final as (
+
+    select 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        payments_aggregated.amount
+    from orders
+    left join payments_aggregated on orders.order_id = payments_aggregated.order_id
+
+)
+
+select * from final
